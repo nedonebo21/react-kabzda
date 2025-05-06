1626ee7b197765c938f083b3993eb2406365ee3f
nedonebo21 <cursed.void10007@gmail.com>
2025-05-06 16:57:24 +0400

    refactor UncontrolledAccordion + callback (07)

--- src/components/UncontrolledAccordion/UncontrolledAccordion.tsx
+++ src/components/UncontrolledAccordion/UncontrolledAccordion.tsx
@@ -9,30 +9,24 @@ export const UncontrolledAccordion = (props: PropsType) => {
     const [collapsed, setCollapsed] = useState<boolean>(false)
 
     const clickCollapsedHandler = () => !collapsed ? setCollapsed(true)
-        : setCollapsed(false)
+        : setCollapsed(!collapsed)
 
     return (
         <div>
-            <UncontrolledAccordionTitle title={props.titleValue}/>
-            <button
-                style={
-                    {width: "50px", height: "30px", fontSize: "16px", color: "white", backgroundColor: "gray"}
-                }
-                onClick={clickCollapsedHandler}
-            >Toggle
-            </button>
+            <UncontrolledAccordionTitle onClick={clickCollapsedHandler} title={props.titleValue}/>
             {!collapsed && <UncontrolledAccordionBody/>}
         </div>
     );
 };
 type AccordionTitlePropsType = {
     title: string,
+    onClick: () => void
 }
 
 function UncontrolledAccordionTitle(props: AccordionTitlePropsType) {
     console.log("AccordionTitle rendering")
     return (
-        <h3>-- {props.title} --</h3>
+        <h3 onClick={props.onClick} style={{cursor: "pointer"}}>-- {props.title} --</h3>
     )
 }
 
