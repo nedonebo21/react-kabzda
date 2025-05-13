168b40841aad6059c15e4bd926f09588e78d487c
nedonebo21 <cursed.void10007@gmail.com>
2025-05-13 12:09:13 +0400

    refactor UncontrolledRating (08)

--- src/components/UncontrolledRating/UncontrolledRating.tsx
+++ src/components/UncontrolledRating/UncontrolledRating.tsx
@@ -7,14 +7,25 @@ export function UncontrolledRating() {
 
     return (
         <div>
-            <Star onClick={() => {setValue(1)}} selected={value > 0}/>
-            <Star onClick={() => {setValue(2)}} selected={value > 1}/>
-            <Star onClick={() => {setValue(3)}} selected={value > 2}/>
-            <Star onClick={() => {setValue(4)}} selected={value > 3}/>
-            <Star onClick={() => {setValue(5)}} selected={value > 4}/>
+            <Star onClick={() => {
+                setValue(1)
+            }} selected={value > 0}/>
+            <Star onClick={() => {
+                setValue(2)
+            }} selected={value > 1}/>
+            <Star onClick={() => {
+                setValue(3)
+            }} selected={value > 2}/>
+            <Star onClick={() => {
+                setValue(4)
+            }} selected={value > 3}/>
+            <Star onClick={() => {
+                setValue(5)
+            }} selected={value > 4}/>
         </div>
     )
 }
+
 
 type StarPropsType = {
     selected: boolean
@@ -23,10 +34,16 @@ type StarPropsType = {
 
 function Star(props: StarPropsType) {
     console.log("star rendering")
-
-    if (props.selected) {
-        return <span onClick={props.onClick} style={{color: "yellow", cursor: "pointer"}}><b>star </b></span>
-    } else {
-        return <span onClick={props.onClick} style={{ cursor: "pointer"}}>star </span>
+    const spanStyleActive = {
+        color: "yellow",
+        cursor: "pointer",
     }
+    const spanStyle = {
+        cursor: "pointer",
+    }
+    return <span
+        onClick={props.onClick}
+        style={props.selected ? spanStyleActive : spanStyle}>
+        {props.selected ? <b>✯ </b> : "✯ "}
+    </span>
 }
